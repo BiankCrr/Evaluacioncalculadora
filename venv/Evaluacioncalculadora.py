99515c9852964f962fe2011d4e9810d5d9983b8e
Biank Ramirez <1079270@horusur.corp>
2023-01-15 10:22:42 -0600

    Se termina definición de clases con clases dependientes de libreria comentadas

--- venv/Evaluacioncalculadora.py
+++ venv/Evaluacioncalculadora.py
@@ -1,14 +1,31 @@
 import numpy as np
 import sys
 
-def Sumar(a, b):
+def Suma(a, b):
     return np.sum(a + b)
 
-def Restar(a, b):
+def Resta(a, b):
     return a - b
 
-def Multiplicar(a, b):
+def Multiplicación(a, b):
     return a * b
 
-def Dividir(a, b):
+def Dividición(a, b):
     return a / b
+
+def Raiz (a, b):
+    return pow (a, 1/b)
+
+def Exponente(a,b):
+    return np.power(a,b)
+#def Seno (a):
+    #return np sin (a)
+
+#def Coseno (a):
+    #return np cos (a)
+
+def Tangente(a):
+    return np.tan(a)
+
+def Close():
+    sys.exit()
