1a827c76fee5692bef79efaa4861cc490216664d
Biank Ramirez <1079270@horusur.corp>
2023-01-15 09:51:17 -0600

    Importación de libreria y definición de operaciones básicas

---
+++ venv/Evaluacioncalculadora.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+
+def Sumar(a, b):
+    return np.sum(a + b)
+
+def Restar(a, b):
+    return a - b
+
+def Multiplicar(a, b):
+    return a * b
+
+def Dividir(a, b):
+    return a / b
