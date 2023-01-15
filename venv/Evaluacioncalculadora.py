e227a3b891b76c91d805392bec6a76c1485aafd7
Biank Ramirez <1079270@horusur.corp>
2023-01-15 10:35:34 -0600

    Se instala la colección libreria numpy y se agrega el menú de la calculadora

--- venv/Evaluacioncalculadora.py
+++ venv/Evaluacioncalculadora.py
@@ -13,19 +13,36 @@ def Multiplicación(a, b):
 def Dividición(a, b):
     return a / b
 
-def Raiz (a, b):
+def Raíz (a, b):
     return pow (a, 1/b)
 
 def Exponente(a,b):
     return np.power(a,b)
-#def Seno (a):
-    #return np sin (a)
+def Seno (a):
+    return np.sin (a)
 
-#def Coseno (a):
-    #return np cos (a)
+def Coseno (a):
+    return np.cos(a)
 
 def Tangente(a):
-    return np.tan(a)
+    return np.tan (a)
 
 def Close():
     sys.exit()
+    
+def Close():
+    sys.exit()
+
+
+def EvaluacionCalculadora():
+    print("Buena vida, ¿Que operación deseas realizar?; ")
+    print("1 Suma")
+    print("2 Resta")
+    print("3 Multiplicación")
+    print("4 División")
+    print("5 Exponente")
+    print("6 Raíz n")
+    print("7 Seno")
+    print("8 Coseno")
+    print("9 Tangente")
+    print("10 Salir de la Calculadora")
